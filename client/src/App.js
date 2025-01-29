e9acbe89fadd943bd457c83de3b29943505d5517
Francis <fransixpaul@yahoo.com>
2025-01-29 20:06:32 +0100

    Enhance code structure and clarity in App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Layout, Image, Typography } from "antd";
-import Logo from "./images/Instaverse.png"
+import { Layout, Image, Typography } from 'antd';
+import Logo from './images/Instaverse.png';
 import Home from './components/Home';
 import styles from './styles';
 
-
-
+// Destructuring to clean up the code
 const { Title } = Typography;
 const { Header, Footer } = Layout;
 
@@ -13,14 +12,16 @@ const App = () => {
     return (
         <Layout style={styles.layout}>
             <Header style={styles.header}>
-                <Image style={styles.image} preview={false} src={Logo}  width={45} />
+                {/* Logo and Title */}
+                <Image style={styles.image} preview={false} src={Logo} width={45} />
                 &nbsp;
                 <Title style={styles.title}>Momentify</Title>
             </Header>
+            {/* Home Component */}
             <Home />
             <Footer style={styles.footer}>2025 Momentify</Footer>
         </Layout>
-    )
+    );
 }
 
 export default App;
